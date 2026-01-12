d98d7e0730b13b22298e6136891b476fadfadd58
Vishnu vijayan <vishnuvijayan90721@gmail.com>
2026-01-12 09:59:32 +0530

    update cors

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://frontend-book-cart-cvig.vercel.app/",
+      "https://frontend-book-cart-cvig.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
